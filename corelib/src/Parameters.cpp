494b18e2de7f79693fd09344bdaf400c0dff0a1a
ChemicalNRG <59423696+ChemicalNRG@users.noreply.github.com>
2021-02-07 21:33:40 +0100

    Update Parameters.cpp

--- corelib/src/Parameters.cpp
+++ corelib/src/Parameters.cpp
@@ -68,8 +68,8 @@ std::string Parameters::createDefaultWorkingDirectory()
 	std::string path = UDirectory::homeDir();
 	if(!path.empty())
 	{
-		UDirectory::makeDir(path += UDirectory::separator() + "$(cat ~/.config/user-dirs.dirs | grep "DOCUMENTS" | cut -d'"' -f2 | cut -d"/" -f2)");
-		UDirectory::makeDir(path += UDirectory::separator() + "RTAB-Map");
+		UDirectory::makeDir(path += UDirectory::separator() + "$(cat ~/.config/user-dirs.dirs | grep 'DOCUMENTS' | cut -d'"' -f2 | cut -d'/' -f2)");
+		UDirectory::makeDir(path += UDirectory::separator() + 'RTAB-Map');
 
 	}
 	else
